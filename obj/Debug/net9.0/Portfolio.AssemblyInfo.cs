b60f4affc0424bdb6a0a56fd8fd645191bbbd5b7
Tuyen99it <nguyentuyen99tnut@gmail.com>
2025-08-17 11:31:23 +0700

    Edit home text

--- obj/Debug/net9.0/Portfolio.AssemblyInfo.cs
+++ obj/Debug/net9.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f6da4aecc29331f4dd1d1b6e3e8b7811ded56f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6e04454610cdf0b729b6e75490f0499b750c609")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
